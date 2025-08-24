5d7ce497d672cbe0228ab7f0efd0bcfd8cec0c62
v3r3tnxv <alexanderveretnov@gmail.com>
2025-08-25 01:44:52 +0300

    29 Добавлен экран загрузски

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,20 @@
 // @/app/loading.tsx
-
+import Image from 'next/image';
+import { ProgressBar } from '@/shared/ui';
+import styles from './Home.module.scss';
 
 export default function Loading() {
     return (
-        <div>
-            <p>Главная Загрузка...</p>
+        <div className={styles.loading}>
+            <Image
+                src="/images/loading-scrine.png"
+                width={100}
+                height={100}
+                alt="Робот"
+                className={styles.loadingImage}
+            />
+            <p className={styles.label}>Загрузка</p>
+            <ProgressBar progress={10} />
         </div>
     );
 }
